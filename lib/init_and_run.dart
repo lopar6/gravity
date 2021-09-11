6396e1ec756db0d43d34b09b86280caa6be5cc62
lopar6 <lopar6@gmail.com>
2021-09-11 12:25:08 -0600

    config adjustment

--- lib/init_and_run.dart
+++ lib/init_and_run.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:gravity/app.dart';
 import 'package:gravity/config.dart';
 import 'package:gravity/pages/home.dart';
+import 'package:gravity/pages/level_page.dart';
 import 'package:gravity/services/app_state.dart';
 import 'package:gravity/widgets/game.dart';
 
@@ -15,7 +16,7 @@ void initAndRun(Config config) {
   AppState.instance.config = config;
 
   //! temp to adjust gameplay
-  runApp(GameWidget(game: GravityGame()));
+  runApp(LevelPage(0));
 
   //! use this for normal functionality
   // runApp(App());
